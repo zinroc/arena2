6cdbceb89fb01c18756634fe4d9885716059d684
zinroc <admin@Admins-MacBook-Air-2.local>
2016-07-15 11:50:18 -0400

    added a dice

--- public/js/encounter_ctrl.js
+++ public/js/encounter_ctrl.js
@@ -23,14 +23,15 @@ app.directive('bsTooltip', function (){
 
     console.log($scope.email, $scope.name);
 
-
+    /** Mock values START**/
     $scope.playerCards = [{suit: 's' , suit_symbol: '<font color="black">&spades;</font>', value: '4'}, {s: 'd', suit_symbol: '<font color="red">&diams;</font>', value: "K"}];
     $scope.tableCards =[{suit: 'c', suit_symbol: '<font color="black">&clubs;</font>', value: '10'}, {suit: 'h', suit_symbol: '<font color="red">&hearts;</font>', value: 'A'}, 
                         {suit: 'h', suit_symbol: '<font color="red">&hearts;</font>', value: '2'}, {suit: 'd', suit_symbol: '<font color="red">&diams;</font>', value: '2'}, 
                         {suit: 'c', suit_symbol: '<font color="black">&clubs;</font>', value: '9'}];
     $scope.opponentCards = [{suit: 's' , suit_symbol: '<font color="black">&spades;</font>', value: 'Q'}, {s: 'd', suit_symbol: '<font color="red">&diams;</font>', value: "Q"}];
+    /** Mock values END ***/
 
-
+    /** Loading START ***/
 
     $scope.load = function(){
         for(var i=0; i<$scope.playerCards.length; i++){
@@ -45,7 +46,39 @@ app.directive('bsTooltip', function (){
             $scope.opponentCards[i].suit_symbol = $sce.trustAsHtml($scope.opponentCards[i].suit_symbol);
         }
     };
+    /** Loading END ***/
 
+    /** Poker Math  START **/
+    $scope.evaluateWinningHand = function(){
+        
+    };
+    /** Poker Math END **/
+
+    /** UI START**/
+
+    $scope.spinDice = "fa-spin";
+    $scope.diceValue = null;
+
+    $scope.stopDice = function (){
+        console.log("stop dice");
+        //pick a value 1-20 for the dice.
+        if (!$scope.diceValue) {
+            $scope.diceValue = Math.ceil(Math.random()*20);
+            console.log($scope.diceValue);
+            $scope.spinDice = "";
+        } else {
+            $scope.diceValue = null;
+            $scope.spinDice = "fa-spin";
+        }
+
+        $scope.evaluateWinningHand();
+
+    };
+
+    /** UI END **/
+
+
+    /** RUN ***/
     $scope.load();
 
 });
