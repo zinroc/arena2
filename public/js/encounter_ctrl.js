3f26708149a78a28ebff3ff3254d7a314f687548
zinroc <admin@Admins-MacBook-Air-2.local>
2016-07-15 01:14:42 -0400

    added ng-cloak and finished card art

--- public/js/encounter_ctrl.js
+++ public/js/encounter_ctrl.js
@@ -22,11 +22,27 @@ app.directive('bsTooltip', function (){
     $scope.name = utils.getCookie("name");
 
     console.log($scope.email, $scope.name);
-    $scope.playerCards = [{suit: '&spades;', value: '4'}, {suit: '&diams;', value: "K"}];
+
+
+    $scope.playerCards = [{suit: 's' , suit_symbol: '<font color="black">&spades;</font>', value: '4'}, {s: 'd', suit_symbol: '<font color="red">&diams;</font>', value: "K"}];
+    $scope.tableCards =[{suit: 'c', suit_symbol: '<font color="black">&clubs;</font>', value: '10'}, {suit: 'h', suit_symbol: '<font color="red">&hearts;</font>', value: 'A'}, 
+                        {suit: 'h', suit_symbol: '<font color="red">&hearts;</font>', value: '2'}, {suit: 'd', suit_symbol: '<font color="red">&diams;</font>', value: '2'}, 
+                        {suit: 'c', suit_symbol: '<font color="black">&clubs;</font>', value: '9'}];
+    $scope.opponentCards = [{suit: 's' , suit_symbol: '<font color="black">&spades;</font>', value: 'Q'}, {s: 'd', suit_symbol: '<font color="red">&diams;</font>', value: "Q"}];
+
+
 
     $scope.load = function(){
         for(var i=0; i<$scope.playerCards.length; i++){
-            $scope.playerCards[i].suit = $sce.trustAsHtml($scope.playerCards[i].suit);
+            $scope.playerCards[i].suit_symbol = $sce.trustAsHtml($scope.playerCards[i].suit_symbol);
+        }
+
+        for(var i=0; i<$scope.tableCards.length; i++){
+            $scope.tableCards[i].suit_symbol = $sce.trustAsHtml($scope.tableCards[i].suit_symbol);
+        }
+
+        for(var i=0; i<$scope.opponentCards.length; i++){
+            $scope.opponentCards[i].suit_symbol = $sce.trustAsHtml($scope.opponentCards[i].suit_symbol);
         }
     };
 
