2b5798af8ae00dfecb6117aab4b1680a6c825d5f
zinroc <admin@Admins-MacBook-Air-3.local>
2016-10-25 15:43:55 -0400

    emergence DB

--- credentials.js
+++ credentials.js
@@ -10,5 +10,8 @@ module.exports = {
 //	PG_CON_STRING: "://mnwaqohqvqmnka:ClePM_AO_QGCGV3GDsXMvABa84@ec2-54-83-36-176.compute-1.amazonaws.com:5432/dfe47966djhp9"
 
 //titan
-	PG_CON_STRING: "postgres://sgaoombqeqmzjx:DyJGGHgSjqWTINH4JZVhgGAVY1@ec2-54-243-125-46.compute-1.amazonaws.com:5432/d8472bf2tbc91f&ssl=true"
+//	PG_CON_STRING: "postgres://sgaoombqeqmzjx:DyJGGHgSjqWTINH4JZVhgGAVY1@ec2-54-243-125-46.compute-1.amazonaws.com:5432/d8472bf2tbc91f&ssl=true"
+//emergence
+	PG_CON_STRING: "postgres://cosyudldjdolpk:Z0qqJa7gymZliw2sf3Jdpp4Z34@ec2-54-235-95-188.compute-1.amazonaws.com:5432/d9fgvijtf72tl2"
+
 };
