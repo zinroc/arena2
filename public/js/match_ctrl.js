cb1d86a8319eb6b9c1e7d69fac3fa70b8b5584e6
zinroc <admin@Admins-MacBook-Air-3.local>
2016-11-25 20:11:13 -0500

    match

---
+++ public/js/match_ctrl.js
@@ -0,0 +1,23 @@
+var app = angular.module("ArenaApp");
+
+app.directive('bsTooltip', function (){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            $(element).hover(function(){
+                // on mouseenter
+                $(element).tooltip('show');
+            }, function(){
+
+
+                // on mouseleave
+                $(element).tooltip('hide');
+            });
+        }
+    };
+}).controller("EncounterCtrl", function encounterCtrl ($scope, api_service, $timeout, $sce) {
+    "use strict";
+
+    $scope.hello_world = "hello world";
+
+});
